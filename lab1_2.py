7bd05ef901739434860276e8b0c364962c7f6b93
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-18 16:41:14 +0300

    Final fix

--- lab1_2.py
+++ lab1_2.py
@@ -2,10 +2,11 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-edges_with_weight = [(1, 2, 10), (1, 4, 5), (2, 3, 3), (2, 4, 9), (2, 5, 11), (2, 6, 5), (3, 6, 10), (4, 5, 6),
-         (4, 7, 15),(4, 8, 13), (5, 6, 10), (5, 8, 5), (6, 8, 7), (6, 9, 8),(7, 8, 4), (8, 9, 8)]
+# nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+nodes = [1, 2, 3, 4, 5]
+# edges_with_weight = [(1, 2, 10), (1, 4, 5), (2, 3, 3), (2, 4, 9), (2, 5, 11), (2, 6, 5), (3, 6, 10), (4, 5, 6),
+# (4, 7, 15),(4, 8, 13), (5, 6, 10), (5, 8, 5), (6, 8, 7), (6, 9, 8),(7, 8, 4), (8, 9, 8)]
+edges_with_weight = [(1, 2, 1), (1, 3, 3), (2, 3, 4), (2, 4, 6), (2, 5, 7), (3, 4, 5), (4, 5, 2)]
 
 G = nx.Graph()
 G1 = nx.Graph()
@@ -15,18 +16,18 @@ G1 = G.copy()
 edges_with_weight1 = edges_with_weight.copy()
 
 layout = nx.spring_layout(G1)
-nx.draw(G1,layout,with_labels=True,node_color='b')
+nx.draw(G1, layout, with_labels=True, node_color='b')
 labels = nx.get_edge_attributes(G1, "weight")
-nx.draw_networkx_edge_labels(G1,pos=layout, edge_labels=labels)
+nx.draw_networkx_edge_labels(G1, pos=layout, edge_labels=labels)
 plt.show()
 
 Skeleton = nx.Graph()
 edges_with_weight2 = []
 
-for i in range(0,G1.number_of_nodes()-1):
+for i in range(0, G1.number_of_nodes() - 1):
     if i == 0:
         min_edge = -1
-        min_edge_weight = edges_with_weight1[0][2]
+        min_edge_weight = 100
 
         for n in range(0, G1.number_of_edges()):
             if min_edge_weight > edges_with_weight1[n][2]:
@@ -50,7 +51,8 @@ for i in range(0,G1.number_of_nodes()-1):
         for t in range(0, G1.number_of_edges()):
             flag1 = Skeleton.has_node(edges_with_weight1[t][0])
             flag2 = Skeleton.has_node(edges_with_weight1[t][1])
-            if ((flag1 == True and flag2 == False) or (flag1 == False and flag2 == True) or (flag1 == False and flag2 == False) or (flag1 == True and flag2 == True)):
+            if ((flag1 == True and flag2 == False) or (flag1 == False and flag2 == True) or (
+                    flag1 == False and flag2 == False)):
                 if min_edge_weight > edges_with_weight1[t][2]:
                     min_edge = t
                     min_edge_weight = edges_with_weight1[t][2]
@@ -93,8 +95,8 @@ print(Skeleton.nodes)
 Skeleton.add_weighted_edges_from(edges_with_weight2)
 
 sumweight = 0
-for x in range(0,len(edges_with_weight2)):
-    sumweight+=edges_with_weight2[x][2]
+for x in range(0, len(edges_with_weight2)):
+    sumweight += edges_with_weight2[x][2]
 print("Skeleton min weight sum: " + str(sumweight))
 
 layout2 = nx.spring_layout(Skeleton)
